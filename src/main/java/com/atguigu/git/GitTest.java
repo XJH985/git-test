5e673aa6bee991e6b6280c6dffc53d996b2704d3
Jing <Jing@qq.com>
2022-11-02 15:23:48 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.atguigu.git;
+
+/**
+ * @author jing
+ * @create 2022-11-02 15:22
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello,git!");
+    }
+}
